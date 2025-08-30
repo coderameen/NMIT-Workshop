a579b013c27ae8410c207e32397071fd3d8e2c0f
coderameen <coderameen@gmail.com>
2025-08-30 12:15:21 +0530

    added code

--- Prizza prediction/app.py
+++ Prizza prediction/app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #initiaze application
 app = Flask(__name__)
+
+#Load your Trained model
+import pickle
+model = pickle.load(open('pizza_model.pkl','rb'))
 
 #GET API(default landing page)
 @app.route("/")
 def home():
     return render_template("index.html")
 
+#POST API
+@app.route("/predict", methods=['POST'])
+def predict():
 
-#GET AII
-@app.route("/about_us")
-def about_us():
-    return "This is about us page"
+    if request.method == 'POST':
+        age = int(request.form['ageinp'])
+        weight = int(request.form['weightinp'])
+        pred = model.predict([[age,weight]])
+        if pred.item() == 1:
+            msg = "Enjoy Pizza"
+            return render_template("index.html", message = msg)
+        else:
+            msg = "Go to GYM"
+            return render_template("index.html", message = msg)
+        
+       
 
 
 if __name__=='__main__':
